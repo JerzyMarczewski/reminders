3f01f1ce926100eec75401149fb05225f088b08c
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2024-01-28 19:03:27 +0100

    changed structure, added addList and addReminder methods

--- src/app/firestore.service.ts
+++ src/app/firestore.service.ts
@@ -3,23 +3,25 @@ import {
   CollectionReference,
   DocumentReference,
   Firestore,
+  Query,
   addDoc,
   collection,
   collectionData,
   query,
   where,
 } from '@angular/fire/firestore';
-import { Observable, Subscription, switchMap } from 'rxjs';
+import { Observable, Subscription, of, switchMap } from 'rxjs';
 import { Reminder } from './reminder.model';
 import { List } from './list.model';
 import { AuthService } from './auth.service';
+import { User } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FirestoreService {
-  lists$: Observable<List[]>;
-  reminders$: Observable<Reminder[]>;
+  lists$!: Observable<List[]>;
+  reminders$!: Observable<Reminder[]>;
 
   listsCollection!: CollectionReference;
   remindersCollection!: CollectionReference;
@@ -28,27 +30,32 @@ export class FirestoreService {
     this.listsCollection = collection(this.firestore, 'lists');
     this.remindersCollection = collection(this.firestore, 'reminders');
 
-    this.lists$ = authService.uid$.pipe(
-      switchMap((uid) => {
-        if (uid) {
+    this.lists$ = authService.user$.pipe(
+      switchMap((user: User | null) => {
+        if (user && user.uid) {
           const userListsQuery = query(
             this.listsCollection,
-            where('userId', '==', uid)
+            where('userId', '==', user.uid)
           );
-          return collectionData(userListsQuery) as Observable<List[]>;
+          return collectionData(userListsQuery, {
+            idField: 'id',
+          }) as Observable<List[]>;
         } else {
           return [];
         }
       })
     );
-    this.reminders$ = authService.uid$.pipe(
-      switchMap((uid) => {
-        if (uid) {
+
+    this.reminders$ = authService.user$.pipe(
+      switchMap((user: User | null) => {
+        if (user && user.uid) {
           const userRemindersQuery = query(
             this.remindersCollection,
-            where('userId', '==', uid)
+            where('userId', '==', user.uid)
           );
-          return collectionData(userRemindersQuery) as Observable<Reminder[]>;
+          return collectionData(userRemindersQuery, {
+            idField: 'id',
+          }) as Observable<Reminder[]>;
         } else {
           return [];
         }
@@ -56,25 +63,43 @@ export class FirestoreService {
     );
   }
 
-  addList() {
-    addDoc(this.listsCollection, <List>{
-      name: 'test',
-      description: 'test',
-      userId: 'test',
-    }).then((documentReference: DocumentReference) => {
-      console.log(documentReference);
-    });
+  addList(name: string, description: string) {
+    if (!this.authService.uid)
+      return console.error(
+        'Could not add a new list, because the user is not authenticated'
+      );
+
+    const newList: Partial<List> = {
+      name: name,
+      description: description,
+      userId: this.authService.uid,
+    };
+
+    return addDoc(this.listsCollection, newList);
   }
-  addReminder() {
-    addDoc(this.remindersCollection, <Reminder>{
-      title: 'test',
-      description: 'test',
-      dueDate: new Date(),
-      completed: false,
-      userId: 'test',
-      listId: 'test',
-    }).then((documentReference: DocumentReference) => {
-      console.log(documentReference);
-    });
+
+  addReminder(
+    title: string,
+    listId: string,
+    completed: boolean = false,
+    description?: string,
+    dueDate?: Date
+  ) {
+    if (!this.authService.uid)
+      return console.error(
+        'Could not add a new list, because the user is not authenticated'
+      );
+
+    const newReminder: Partial<Reminder> = {
+      title: title,
+      completed: completed,
+      userId: this.authService.uid,
+      listId: listId,
+    };
+
+    if (description) newReminder.description = description;
+    if (dueDate) newReminder.dueDate = dueDate;
+
+    return addDoc(this.remindersCollection, newReminder);
   }
 }
