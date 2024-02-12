a37d65362238dedcefd9037bdfab757a22b88d84
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2024-02-12 21:46:09 +0100

    added sign up and sign in pages

---
+++ src/app/sign-in-page/sign-in-page.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-sign-in-page',
+  templateUrl: './sign-in-page.component.html',
+  styleUrl: './sign-in-page.component.scss',
+})
+export class SignInPageComponent {
+  signInFormGroup!: FormGroup;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.signInFormGroup = new FormGroup({
+      email: new FormControl<string>('', [
+        Validators.required,
+        Validators.email,
+      ]),
+      password: new FormControl<string>('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
+  }
+
+  get email() {
+    return this.signInFormGroup.get('email');
+  }
+
+  get password() {
+    return this.signInFormGroup.get('password');
+  }
+
+  onSubmit() {
+    if (this.signInFormGroup.valid && this.email && this.password)
+      return this.authService.signInUser(this!.email, this.password);
+  }
+}
