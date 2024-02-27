4e28bc674a6a1a8a38a93d1c77541e2a3cf462de
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2024-02-27 23:15:19 +0100

    changed methods to return observables

--- src/app/storage.service.ts
+++ src/app/storage.service.ts
@@ -1,76 +1,81 @@
 import { Injectable } from '@angular/core';
 import { Storage, getDownloadURL, listAll, ref } from '@angular/fire/storage';
-import { Observable, from, map, of, shareReplay, switchMap } from 'rxjs';
+import { BehaviorSubject, Observable, from, map, of, switchMap } from 'rxjs';
 import { AuthService } from './auth.service';
 import { User } from '@angular/fire/auth';
-
-export interface Avatar {
-  path: string;
-  url: string;
-}
+import { Avatar } from './avatar.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StorageService {
+  currentUser$!: BehaviorSubject<User | null>;
+
   constructor(
     private readonly storage: Storage,
     private authService: AuthService
-  ) {}
+  ) {
+    this.currentUser$ = this.authService.getCurrentUser$();
+  }
 
   getUserAvatar(): Observable<Avatar | null> {
-    return this.authService.user$.pipe(
-      switchMap((user: User | null) => {
+    return this.currentUser$.pipe(
+      switchMap((user) => {
         if (!user) {
-          return of(null);
+          return of(null); // No user, return null avatar
         }
 
         const avatarImgPath = user.photoURL || null;
         if (!avatarImgPath) {
-          return of(null);
+          return of(null); // User has no avatar, return null avatar
         }
 
         const avatarImgRef = ref(this.storage, avatarImgPath);
         return from(getDownloadURL(avatarImgRef)).pipe(
           map(
-            (avatarImgUrl: string) =>
+            (avatarImgUrl) =>
               ({
                 path: avatarImgPath,
                 url: avatarImgUrl,
               } as Avatar)
           )
         );
-      }),
-      shareReplay({ bufferSize: 1, refCount: true })
+      })
     );
   }
 
-  getAllAvatars(): Promise<Avatar[]> {
-    return new Promise<Avatar[]>((resolve, reject) => {
-      const folderPath = 'avatars';
-      const folderRef = ref(this.storage, folderPath);
-      const avatarsData: { path: string; url: string }[] = [];
+  private getAllAvatars(): Observable<Avatar[]> {
+    const folderPath = 'avatars';
+    const folderRef = ref(this.storage, folderPath);
 
-      listAll(folderRef)
-        .then((result) => {
-          const promises = result.items.map((itemRef) => {
-            return getDownloadURL(itemRef).then((url) => {
-              avatarsData.push({
-                path: itemRef.fullPath,
-                url: url,
-              });
-            });
+    return from(
+      listAll(folderRef).then((result) => {
+        const promises = result.items.map((itemRef) => {
+          return getDownloadURL(itemRef).then((url) => {
+            return {
+              path: itemRef.fullPath,
+              url: url,
+            } as Avatar;
           });
-
-          return Promise.all(promises);
-        })
-        .then(() => {
-          resolve(avatarsData);
-        })
-        .catch((error) => {
-          console.error('Error while fetching avatars', error);
-          reject(error);
         });
-    });
+
+        return Promise.all(promises);
+      })
+    );
+  }
+
+  getAllAvatarsSorted(): Observable<Avatar[]> {
+    const avatars$ = this.getAllAvatars();
+
+    return avatars$.pipe(
+      map((avatars) =>
+        avatars.sort((avatar1, avatar2) => {
+          const avatar1Index = parseInt(avatar1.path.split('/')[2]);
+          const avatar2Index = parseInt(avatar2.path.split('/')[2]);
+
+          return avatar1Index - avatar2Index;
+        })
+      )
+    );
   }
 }
