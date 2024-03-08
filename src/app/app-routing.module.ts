76838a53a7e0429486a82bcd2a0acf60833737d7
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2024-03-08 20:17:39 +0100

    updated routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { RegisterPageComponent } from './register-page/register-page.component';
+import { SignInPageComponent } from './sign-in-page/sign-in-page.component';
+import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
-  { path: 'login', component: LoginPageComponent },
-  { path: 'register', component: RegisterPageComponent },
+  { path: 'sign-up', component: SignUpPageComponent },
+  { path: 'sign-in', component: SignInPageComponent },
 ];
 
 @NgModule({
