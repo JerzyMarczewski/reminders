f58c5f0624e25e327e33790315eff19bf6704983
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2024-03-13 20:05:07 +0100

    added verify email page path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { SignInPageComponent } from './sign-in-page/sign-in-page.component';
 import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
+import { VerificationPageComponent } from './verification-page/verification-page.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'sign-up', component: SignUpPageComponent },
   { path: 'sign-in', component: SignInPageComponent },
+  { path: 'verify-email', component: VerificationPageComponent },
 ];
 
 @NgModule({
