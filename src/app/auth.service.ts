06b35e726fc86e36290df1b82132a117081c3dfd
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2024-03-29 22:09:06 +0100

    updated sign in logic

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
+import { FirebaseError } from '@angular/fire/app';
 import {
   Auth,
   User,
   createUserWithEmailAndPassword,
+  deleteUser,
+  sendEmailVerification,
   signInWithEmailAndPassword,
   updateProfile,
   user,
@@ -16,60 +19,91 @@ import { BehaviorSubject, Observable } from 'rxjs';
 export class AuthService {
   user$: Observable<User | null>;
   private authErrorSubject = new BehaviorSubject<string | null>(null);
-  authError$ = this.authErrorSubject.asObservable();
-  private currentUserSubject = new BehaviorSubject<User | null>(null);
+  currentUser$ = new BehaviorSubject<User | null>(null);
 
   constructor(public auth: Auth, private snackBar: MatSnackBar) {
     this.user$ = user(auth);
-    this.user$.subscribe(this.currentUserSubject);
+    this.user$.subscribe(this.currentUser$);
   }
 
-  getCurrentUser$(): BehaviorSubject<User | null> {
-    return this.currentUserSubject;
-  }
-
-  createUser(username: string, email: string, password: string) {
+  signUpUser(username: string, email: string, password: string): void {
     createUserWithEmailAndPassword(this.auth, email, password)
       .then((userCredential) => {
         const user = userCredential.user;
         const defaultAvatarPath = 'avatars/0.png';
 
-        return updateProfile(user, {
-          displayName: username,
-          photoURL: defaultAvatarPath,
-        });
+        return Promise.all([
+          updateProfile(user, {
+            displayName: username,
+            photoURL: defaultAvatarPath,
+          }),
+          sendEmailVerification(user),
+        ]);
+      })
+      .then(() => {
+        // display that mail has been sent
+        console.log('Verification email sent.');
       })
       .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.error(errorMessage);
+        console.error('Error signing up user:', error);
+        console.log(error.code);
+        this.displayError(error);
       });
   }
 
-  signInUser(email: string, password: string) {
-    signInWithEmailAndPassword(this.auth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        // ! DELETE LATER
-        console.log('User logged in');
-      })
-      .catch((error) => this.displayError(error));
+  isUserVerified(): Promise<boolean> {
+    return new Promise<boolean>((resolve, reject) => {
+      const user = this.auth.currentUser;
+      if (!user) {
+        reject(new Error('User is not signed in.'));
+        return;
+      }
+
+      user
+        .reload()
+        .then(() => {
+          const reloadedUser = this.auth.currentUser;
+          resolve(reloadedUser ? reloadedUser.emailVerified : false);
+        })
+        .catch((error) => {
+          console.error('Error checking email verification status:', error);
+          resolve(false);
+        });
+    });
+  }
+
+  sendVerificationEmail(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      const user = this.auth.currentUser;
+      if (!user) {
+        reject(new Error('User is not signed in.'));
+        return;
+      }
+
+      sendEmailVerification(user)
+        .then(() => {
+          resolve();
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  }
+
+  signInUser(email: string, password: string): void {
+    signInWithEmailAndPassword(this.auth, email, password).catch((error) =>
+      this.displayError(error)
+    );
   }
 
   signOutUser() {
-    this.auth
-      .signOut()
-      .then(() => {
-        // ! change later
-        console.log('User logged out successfully');
-      })
-      .catch((error) => {
-        console.error('Error while logging out:', error);
-      });
+    this.auth.signOut().catch((error) => {
+      console.error('Error while logging out:', error);
+    });
   }
 
   editProfile(username: string, avatarPath: string): Promise<void> {
-    return new Promise((resolve, reject) => {
+    return new Promise<void>((resolve, reject) => {
       const user = this.auth.currentUser;
       if (!user) {
         reject(new Error('User is not signed in.'));
@@ -87,7 +121,7 @@ export class AuthService {
             photoURL: avatarPath,
           };
 
-          this.currentUserSubject.next(updatedUser);
+          this.currentUser$.next(updatedUser);
           resolve();
         })
         .catch((error) => {
@@ -97,11 +131,35 @@ export class AuthService {
     });
   }
 
-  private displayError(error: any) {
+  // ! add relogging before deleting
+  deleteUser(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      const user = this.auth.currentUser;
+      if (!user) {
+        reject(new Error('User is not signed in.'));
+        return;
+      }
+
+      deleteUser(user)
+        .then(() => {
+          resolve();
+        })
+        .catch((error) => {
+          console.error('Error deleting user:', error);
+          reject(error);
+        });
+    });
+  }
+
+  private displayError(error: FirebaseError) {
     let errorMessage = 'An error occurred during authentication.';
 
     if (error.code === 'auth/invalid-credential') {
       errorMessage = 'Invalid email or password.';
+    }
+
+    if (error.code === 'auth/email-already-in-use') {
+      errorMessage = 'Email already in use.';
     }
 
     this.authErrorSubject.next(errorMessage);
