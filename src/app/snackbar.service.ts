4faf72db3a1085d7c9bbcabf28a3114fdb1eedec
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2024-04-05 21:13:44 +0200

    updated the displayFirebaseMethod to show a custom message or display message based on error code

--- src/app/snackbar.service.ts
+++ src/app/snackbar.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { FirebaseError } from '@angular/fire/app';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
@@ -12,7 +13,42 @@ export class SnackbarService {
       duration: 5000,
       horizontalPosition: 'center',
       verticalPosition: 'top',
-      panelClass: ['error-snackbar'],
+    });
+  }
+
+  displayFirebaseError(
+    error: FirebaseError,
+    action: string = 'Close',
+    message?: string
+  ): void {
+    if (message) {
+      this.snackBar.open(message, action, {
+        duration: 5000,
+        horizontalPosition: 'center',
+        verticalPosition: 'top',
+      });
+      return;
+    }
+
+    let errorMessage = 'A Firebase error occurred.';
+
+    if (
+      error.code === 'auth/invalid-email' ||
+      error.code === 'auth/missing-password' ||
+      error.code === 'auth/invalid-credential'
+    )
+      errorMessage = 'Invalid credentials';
+
+    if (error.code === 'auth/email-already-in-use')
+      errorMessage = 'Email already in use.';
+
+    if (error.code === 'auth/too-many-requests')
+      errorMessage = 'Too many firebase requests.';
+
+    this.snackBar.open(errorMessage, action, {
+      duration: 5000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
     });
   }
 }
