1c46027d4b81f118b93fb7e64c9ade52101c1142
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2024-04-12 21:53:47 +0200

    completed the confirmation dialog component

--- src/app/confirmation-dialog/confirmation-dialog.component.ts
+++ src/app/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-confirmation-dialog',
@@ -8,7 +8,7 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 })
 export class ConfirmationDialogComponent {
   constructor(
-    private dialogRef: MatDialogRef<ConfirmationDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: { message: string }
+    @Inject(MAT_DIALOG_DATA)
+    public data: { message: string }
   ) {}
 }
