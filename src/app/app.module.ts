e152146faffa63d6298200673a7e56bb3fb41726
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2024-04-12 21:58:13 +0200

    updated used modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,22 +15,65 @@ import {
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 import { environment } from '../environments/environment';
-import { FirestoreService } from './firestore.service';
 import { HomePageComponent } from './home-page/home-page.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { RegisterPageComponent } from './register-page/register-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatNativeDateModule } from '@angular/material/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+import { AddListDialogComponent } from './add-list-dialog/add-list-dialog.component';
+import { AddReminderDialogComponent } from './add-reminder-dialog/add-reminder-dialog.component';
+import { HighlightDirective } from './highlight.directive';
+import { ListPanelComponent } from './list-panel/list-panel.component';
+import { SelectedListPanelComponent } from './selected-list-panel/selected-list-panel.component';
+import { EditListDialogComponent } from './edit-list-dialog/edit-list-dialog.component';
+import { EditReminderDialogComponent } from './edit-reminder-dialog/edit-reminder-dialog.component';
+import { ReminderComponent } from './reminder/reminder.component';
+import { ListComponent } from './list/list.component';
+import { SignInPageComponent } from './sign-in-page/sign-in-page.component';
+import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
+import { EditUserProfileDialogComponent } from './edit-user-profile-dialog/edit-user-profile-dialog.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ConfirmationDialogComponent } from './confirmation-dialog/confirmation-dialog.component';
+import { VerificationPageComponent } from './verification-page/verification-page.component';
+import { ReenterCredentialsDialogComponent } from './reenter-credentials-dialog/reenter-credentials-dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    LoginPageComponent,
-    RegisterPageComponent,
+    AddListDialogComponent,
+    AddReminderDialogComponent,
+    HighlightDirective,
+    ListPanelComponent,
+    SelectedListPanelComponent,
+    EditListDialogComponent,
+    EditReminderDialogComponent,
+    ReminderComponent,
+    ListComponent,
+    SignInPageComponent,
+    SignUpPageComponent,
+    EditUserProfileDialogComponent,
+    NavbarComponent,
+    ConfirmationDialogComponent,
+    VerificationPageComponent,
+    ReenterCredentialsDialogComponent,
   ],
   imports: [
     BrowserModule,
@@ -44,6 +88,23 @@ import { MatListModule } from '@angular/material/list';
     MatButtonModule,
     MatDividerModule,
     MatListModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonToggleModule,
+    MatIconModule,
+    MatCardModule,
+    MatMenuModule,
+    MatDialogModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatProgressSpinnerModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatBadgeModule,
+    MatSnackBarModule,
   ],
   providers: [ScreenTrackingService, UserTrackingService],
   bootstrap: [AppComponent],
