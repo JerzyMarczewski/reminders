ea691522c296b3dd3d5d8d860019beecccf67326
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2024-04-13 14:08:56 +0200

    updated the sign in and sign up pages

--- src/app/sign-up-page/sign-up-page.component.ts
+++ src/app/sign-up-page/sign-up-page.component.ts
@@ -1,14 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
-import {
-  AbstractControl,
-  FormControl,
-  FormGroup,
-  ValidationErrors,
-  ValidatorFn,
-  Validators,
-} from '@angular/forms';
-import { user } from '@angular/fire/auth';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { confirmPasswordValidator } from '../validators';
 
 @Component({
   selector: 'app-sign-up-page',
@@ -21,38 +14,32 @@ export class SignUpPageComponent {
   constructor(private authService: AuthService) {}
 
   ngOnInit() {
-    this.signUpFormGroup = new FormGroup(
-      {
-        username: new FormControl<string>('', [Validators.required]),
-        email: new FormControl<string>('', [
-          Validators.required,
-          Validators.email,
-        ]),
-        password: new FormControl<string>('', [
-          Validators.required,
-          Validators.minLength(6),
-        ]),
-        confirmPassword: new FormControl<string>('', [
-          Validators.required,
-          Validators.minLength(6),
-        ]),
-      },
-      { validators: this.passwordsMatchValidator }
-    );
+    this.signUpFormGroup = new FormGroup({
+      username: new FormControl<string>('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(25),
+        Validators.pattern('[a-zA-Z0-9]*'),
+      ]),
+      email: new FormControl<string>('', [
+        Validators.required,
+        Validators.email,
+      ]),
+      password: new FormControl<string>('', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.pattern(/^(?=.*\d)(?=.*[a-zA-Z]).{8,}$/),
+      ]),
+      confirmPassword: new FormControl<string>('', [
+        Validators.required,
+        confirmPasswordValidator('password'),
+      ]),
+    });
+
+    this.signUpFormGroup.get('password')?.valueChanges.subscribe(() => {
+      this.signUpFormGroup.get('confirmPassword')?.updateValueAndValidity();
+    });
   }
-
-  passwordsMatchValidator: ValidatorFn = (
-    control: AbstractControl
-  ): ValidationErrors | null => {
-    const password = control.get('password');
-    const confirmPassword = control.get('confirmPassword');
-
-    return password &&
-      confirmPassword &&
-      password.value === confirmPassword.value
-      ? null
-      : { passwordsNotMatch: true };
-  };
 
   onSubmit() {
     const username = this.signUpFormGroup.controls?.['username'].value;
@@ -60,7 +47,7 @@ export class SignUpPageComponent {
     const password = this.signUpFormGroup.controls?.['password'].value;
 
     if (this.signUpFormGroup.valid && username && email && password) {
-      this.authService.createUser(username, email, password);
+      this.authService.signUpUser(username, email, password);
     }
   }
 }
